edd416eaf130b787a2642ce43421dbfc1f5be0df
ENoir420 <70651562+ENoir420@users.noreply.github.com>
2021-06-19 14:32:58 +0200

    Update

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.AllLogs = true											-- Attiva/Disattiva i logs
 Config.postal = false  											-- se lo setti False, non apparirà il civico di spawn
-Config.username = "Bot Log" 							-- Bot Username
+Config.username = "Phoenix Log" 							-- Bot Username
 Config.avatar = "https://placeholder.com/"				-- Bot Avatar
 Config.communtiyName = "Phoenix RP"					-- Nome al top embed
 Config.communtiyLogo = "https://placeholder.com/"		-- Icona al top embed
@@ -13,9 +13,9 @@ Config.FooterIcon = "https://placeholder.com/"			-- Icona Footer per l' embed
 Config.weaponLog = true  			-- setta False per disabilitare i colpi d'arma da fuoco log
 Config.InlineFields = true			-- setta False per disabilitare i dettagli dei player
 
-Config.playerID = false				-- setta False per disabilitare il Player ID nei Log
+Config.playerID = true				-- setta False per disabilitare il Player ID nei Log
 Config.steamID = true				-- setta False per disabilitare lo Steam ID nei Log
-Config.steamURL = false				-- setta False per disabilitare lo URL Steam nei Log
+Config.steamURL = true				-- setta False per disabilitare lo URL Steam nei Log
 Config.discordID = true				-- setta False per disabilitare il Discord ID nei Log
 Config.license = false				-- setta False per disabilitare il numero di licenza nei Log
 Config.IP = false					-- setta False per disabilitare l'indirizzo IP nei Log
@@ -34,11 +34,11 @@ Config.BaseColors ={
 Config.webhooks = {		
 	all = "DISCORD_WEBHOOK",		-- Tutti i log in un unico canale
 	chat = "https://discord.com/api/webhooks/855611974058377228/xeXRIc7eexnm9Ys7lAdtsWdpANQ9Usg-6hloMK_8w92gTDjBY1T_ezjAnc4ydgdGcppc",		-- Chat Message
-	joins = "https://discord.com/api/webhooks/855611628946456586/LoZmJUH3QzPjc-e_URfsFCLiS2NxTEr9M1A36dFPABSc2_5pPd7FD4NQnYoC0Xj1HLNA",		-- Log-in Player
-	leaving = "https://discord.com/api/webhooks/855611628946456586/LoZmJUH3QzPjc-e_URfsFCLiS2NxTEr9M1A36dFPABSc2_5pPd7FD4NQnYoC0Xj1HLNA",	-- Log-out Player
-	deaths = "https://discord.com/api/webhooks/855611853043793930/oCLMu6xZgbZ8O6vRoJ-SZYcCgB1fTbk8-34UNyf1rd-T2NenNxQC8Dw136TLykLIMW2I",		-- Player che spara
-	shooting = "DISCORD_WEBHOOK",	-- Player Morto
-	resources = "https://discord.com/api/webhooks/855615988141391882/jHIpJptNwCMagtjvg2_txtjby535vhBygLj2DwCgxAdAfQlF0fQlPnTAcG9MULqG4dJf",	-- Risorsa Startata/Stoppata
+	joins = "https://discord.com/api/webhooks/855786495168479262/0zguo2VU9oVToWdsgtTk2MyNfV3swDe_fSYZSwfC08ir48XN7yMxJuCSryIycQF-KEeN",		-- Log-in Player
+	leaving = "https://discord.com/api/webhooks/855786495168479262/0zguo2VU9oVToWdsgtTk2MyNfV3swDe_fSYZSwfC08ir48XN7yMxJuCSryIycQF-KEeN",	-- Log-out Player
+	deaths = "https://discord.com/api/webhooks/855786601450700801/3K2VFquCtTLKFBvE00D-ocBpSA2m0aTAD02PjLJ7s-CGXeWEUub_-MWF33G98HXgIdst",		-- Player che spara
+	shooting = "https://discord.com/api/webhooks/855786601450700801/3K2VFquCtTLKFBvE00D-ocBpSA2m0aTAD02PjLJ7s-CGXeWEUub_-MWF33G98HXgIdst",	-- Player Morto
+	resources = "DISCORD_WEBHOOK",	-- Risorsa Startata/Stoppata
 }
 
 Config.TitleIcon = {		
